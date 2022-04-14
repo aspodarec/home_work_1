02276ce97b492d792df97c29c8613ddbac67a2b4
spodarec <anastasiaspodarec@gmail.com>
2022-04-14 20:04:45 +0300

    Task_6.1-6.5

--- Task_5.6.py
+++ Task_5.6.py
@@ -3,4 +3,4 @@ with open('file_6.txt', 'r') as init_f:
     for line in init_f:
         subject, lecture, practice, lab = line.split()
         subj[subject] = int(lecture) + int(practice) + int(lab)
-    print(f'Общее количество часов по предмету - \n {subj}')
+    print(f'Общее количество часов по предмету - \n {subj}'
---
+++ Task_6.5.py
@@ -0,0 +1,30 @@
+class Pen(Stationary):
+    def __init__(self, title):
+        super().__init__(title)
+
+    def draw(self):
+        return f'Вы взяли {self.title}. Запуск отрисовки ручкой'
+
+
+class Pencil(Stationary):
+    def __init__(self, title):
+        super().__init__(title)
+
+    def draw(self):
+        return f'Вы взяли {self.title}. Запуск отрисовки карандашом'
+
+
+class Handle(Stationary):
+    def __init__(self, title):
+        super().__init__(title)
+
+    def draw(self):
+        return f'Вы взяли {self.title}. Запуск отрисовки маркером'
+
+
+pen = Pen('Ручка')
+pencil = Pencil('Карандаш')
+handle = Handle('Маркер')
+print(pen.draw())
+print(pencil.draw())
+print(handle.draw())
